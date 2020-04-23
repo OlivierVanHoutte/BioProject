99e1a98fcea5ce98858031637e13fd2cd752c2c0
OlivierVanHoutte <olivier.van.houtte.sonic@gmail.com>
2020-04-23 10:36:17 +0200

    Made a better _genMix2()

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
-# bitwise conversion
+# mapping voor bitwise conversion
 mappings = {}
 mappings['00'] = 'A'
 mappings['01'] = 'T'
 mappings['10'] = 'C'
 mappings['11'] = 'G'
 
+# bitwise conversion
 def string_to_DNA_Bitwise(str):
 	result = ''
 	for i in str:
@@ -18,28 +19,28 @@ def string_to_DNA_Bitwise(str):
 			result += DNA_Char
 	return result
 
-# mogenlijke Stuff in geval van 1 DNA character per real character
+# mogenlijke Stuff in geval van 1 DNA character per real character (15 opties)
 mix = {}
-mix['A'] = ['A']
-mix['B'] = ['A', 'T']
-mix['C'] = ['C']
-mix['D'] = ['A', 'C']
-mix['E'] = ['A', 'T', 'C']
-mix['F'] = ['A', 'C', 'G']
-mix['G'] = ['G']
-mix['H'] = ['A', 'G']
-mix['I'] = ['A', 'T', 'G']
-mix['J'] = ['C', 'G']
-mix['K'] = ['T', 'C']
-mix['L'] = ['T', 'C', 'G']
-mix['M'] = ['T', 'G']
-mix['N'] = ['A', 'T', 'C', 'G']
+mix['A'] = [['A']]
+mix['B'] = [['A', 'T']]
+mix['C'] = [['C']]
+mix['D'] = [['A', 'C']]
+mix['E'] = [['A', 'T', 'C']]
+mix['F'] = [['A', 'C', 'G']]
+mix['G'] = [['G']]
+mix['H'] = [['A', 'G']]
+mix['I'] = [['A', 'T', 'G']]
+mix['J'] = [['C', 'G']]
+mix['K'] = [['T', 'C']]
+mix['L'] = [['T', 'C', 'G']]
+mix['M'] = [['T', 'G']]
+mix['N'] = [['A', 'T', 'C', 'G']]
 mix['O'] = ['#']
 mix['P'] = ['#']
 mix['Q'] = ['#']
 mix['R'] = ['#']
 mix['S'] = ['#']
-mix['T'] = ['T']
+mix['T'] = [['T']]
 mix['U'] = ['#']
 mix['V'] = ['#']
 mix['W'] = ['#']
@@ -48,12 +49,13 @@ mix['Y'] = ['#']
 mix['Z'] = ['#']
 mix[' '] = ['#']
 
+# Old
 def _genMix(left, count, mixchar):
 	DNA_char_r = {'A', 'C', 'T', 'G'}
 	while (len(DNA_char_r)  > 0 ):
 		r = DNA_char_r.pop()
 		DNA_char_r.add(r)
-		cright = []
+		cright = [r]
 		for j in DNA_char_r.copy():
 			if j not in cright:
 				cright.append(j)
@@ -65,15 +67,31 @@ def _genMix(left, count, mixchar):
 				_genMix(t ,count-1, mixchar)
 		DNA_char_r.remove(r)
 
+# New (and improved?)
+def _genMix2(mixchar, count = 1, l = []):
+
+	import itertools
+	x = ['A', 'C', 'T', 'G']
+	for i1 in range(len(x)):
+		for c1 in [c1 for c1 in itertools.combinations(x, i1+1)]:
+			l1 = list(c1), [i1 for i1 in x if not i1 in c1]
+			l2 = l.copy()
+			l2.append(l1[0])
+			if count == 0:
+				mix[next(mixchar)] = l2
+			else:
+				_genMix2(mixchar, count-1, l2)
+		
 def genMix(count):	
 	mixchar = iter(mix.keys())
 	try:
-		_genMix([],count - 1, mixchar)
+		_genMix2(mixchar, count-1)
 	except Exception as e:
 		print('-- 2 per character --', e)
 	for i in mix:
 		print(i, mix[i])
 
+# Generate a list of DNA strings fitting the given string
 def genDNAStrings(_str, _amount):
 	results = []
 	while _amount > 0:
@@ -81,6 +99,7 @@ def genDNAStrings(_str, _amount):
 		_amount -= 1
 	return results
 
+# Generate a DNA string fitting the given string 
 def genDNAString(_str):
 	import random
 	result = ''
@@ -90,6 +109,7 @@ def genDNAString(_str):
 			result += l[index]
 	return result
 
+# Convert DNA codons to possible characters
 def getPossibleInput(koppels):
 	import math
 	total_score = -math.inf
@@ -131,10 +151,10 @@ def getPossibleInput(koppels):
 	return choice		
 
 if __name__ == "__main__":
-	print(string_to_DNA_Bitwise("Hallo ik ben olivier")) #TACATCATTCGATCGATCGGACAATCCTTCCGACAATCACTCTTTCGCACAATCGGTCGATCCTTGTCTCCTTCTTTGAC
-	koppelSize = 1
+	print(string_to_DNA_Bitwise("Hallo ik ben Olivier")) #TAATTAACTAAGTATATATTTATCTATGTACATACTTACCTACGTAGATAGTTAGCTAGGTTAATTTA
+	koppelSize = 2
 	genMix(koppelSize)
-	res = genDNAStrings("abcdefg", 9) #['GAGGGGGGTAAAGATGACGGGATGTCTAGGGAATGCGTGA', 'GAGGGGTTTAAAGCGGGCGGGAGTAATTGGGAATGCGATA', 'GAGGGGTTGATCGATGTTGTGTGCGCTATGGCAGGAGTTA', 'GAGGTTTTTTTTGCTGGTGTGTGTAATTGGGAATGCGTTA']
+	res = genDNAStrings("Hallo ik ben Olivier", 80)
 	print(res)
 	result = ''
 	i = 0
